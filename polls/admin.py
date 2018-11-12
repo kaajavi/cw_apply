6274072022eb90e0361058f3d957df1fc2dd4bc9
kaajavi <javierguignard@gmail.com>
2018-11-12 12:22:50 -0300

    Add inline choice in admin.

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    readonly_fields = ('votes',) #Not change this on admin site
+    extra = 0
+
+
+class QuestionAdmin(admin.ModelAdmin):
+        search_fields = ('question_text',)
+        list_display = ('question_text','pub_date')
+        inlines = [ChoiceInline, ]
+
+
+admin.site.register(Question,QuestionAdmin)
