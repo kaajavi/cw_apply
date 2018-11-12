7fe33e63529a85f2acc71b58f00ea2d12db0cbbf
kaajavi <javierguignard@gmail.com>
2018-11-12 16:27:29 -0300

    Add bootstrap on frontend

---
+++ polls/static/js/polls.js
@@ -0,0 +1,17 @@
+(function ($) {
+  "use strict";
+
+  // Control buttons
+  $('.next').click(function () {
+    $('.carousel').carousel('next');
+    return false;
+  });
+  $('.prev').click(function () {
+    $('.carousel').carousel('prev');
+    return false;
+  });
+
+  // On carousel scroll
+
+})
+(jQuery);
