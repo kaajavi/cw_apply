60fbf610886302e7ecdcf86abc8bcad328752998
kaajavi <javierguignard@gmail.com>
2018-11-12 16:38:26 -0300

    Add test cases

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,8 @@ class Question(models.Model):
         return self.choices.all().order_by('votes').last()
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - timedelta(days=1)
+        now = timezone.now()
+        return now - timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
