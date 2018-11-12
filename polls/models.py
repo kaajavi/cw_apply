77be932d86119bd2fcb49109f1cd3dbde79fd46d
kaajavi <javierguignard@gmail.com>
2018-11-12 16:26:35 -0300

    Add related_name on models fk

--- polls/models.py
+++ polls/models.py
@@ -2,9 +2,16 @@ from django.db import models
 from django.utils import timezone
 from datetime import timedelta
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def choice_win(self):
+        '''
+        :return: winner choice
+        '''
+        return self.choices.all().order_by('votes').last()
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - timedelta(days=1)
@@ -14,11 +21,15 @@ class Question(models.Model):
 
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choices')
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    def is_win(self):
+        '''
+        :return: True if this choice is win
+        '''
+        return self.votes >= self.question.choice_win().votes
+
     def __str__(self):
         return self.choice_text
-
-
